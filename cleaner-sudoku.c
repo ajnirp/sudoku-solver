b2287d34d8a2bb71edc456b268585227bdfe870d
Rohan Prinja <rohan.prinja@gmail.com>
2025-08-19 21:40:58 -0700

    Use ints instead of chars

--- cleaner-sudoku.c
+++ cleaner-sudoku.c
@@ -6,10 +6,15 @@
 
 #define NUM_TESTCASES 6
 
-// Boards are stored as 82-character strings. The 82nd character is '\0'.
-// '0' represents a blank square. '1' represents the number 1, '2' represents
-// the number 2, and so on.
-char at(char board[82], int r, int c) { return board[r*9 + c]; }
+// Boards are stored as arrays of 81 ints. 0 represents a blank square.
+
+void print(const int board[81]) {
+    for (int i = 0; i < 81; i++) {
+        printf("%d", board[i]);
+    }
+}
+
+char at(int board[81], int r, int c) { return board[r*9 + c]; }
 
 // Set index i of `result` to `true` if the cell at (r, c) in `board` cannot
 // be set to the character representing the number i. Else, leave it untouched.
@@ -17,19 +22,19 @@ char at(char board[82], int r, int c) { return board[r*9 + c]; }
 //
 // For example, if there is a 5 at row (2, 3) then
 // `find_disallowed(board, 2, 7, result)` will set `result[5]` to `false`.
-bool find_disallowed(char board[82], int i, bool result[10]) {
+bool find_disallowed(int board[81], int i, bool result[10]) {
     int r = i/9, c = i%9;
     for (int _r = 0; _r < 9; _r++) {
-        result[at(board, _r, c) - '0'] = true;
+        result[at(board, _r, c)] = true;
     }
     for (int _c = 0; _c < 9; _c++) {
-        result[at(board, r, _c) - '0'] = true;
+        result[at(board, r, _c)] = true;
     }
     int tlr = r - r%3;
     int tlc = c - c%3;
     for (int _r = tlr; _r < tlr + 3; _r++) {
         for (int _c = tlc; _c < tlc + 3; _c++) {
-            result[at(board, _r, _c) - '0'] = true;
+            result[at(board, _r, _c)] = true;
         }
     }
 }
@@ -38,7 +43,7 @@ bool find_disallowed(char board[82], int i, bool result[10]) {
 bool same_box(int i, int j) { return i/27 == j/27 && (i%9)/3 == (j%9)/3; }
 
 // True if and only if this is a solved board.
-bool solved(char board[82]) {
+bool solved(int board[81]) {
     for (int i = 0; i < 81; i++) {
         for (int j = 0; j < 81; j++) {
             if (i == j) { continue; }
@@ -52,29 +57,44 @@ bool solved(char board[82]) {
 }
 
 // Backtracking helper function.
-bool helper(char board[82], int i) {
+bool helper(int board[81], const int i) {
     if (i == 82) {
         return true;
     }
-    if (board[i] != '0') {
+    if (board[i] != 0) {
         return helper(board, i+1);
     }
     bool cant_use[10] = {false};
     find_disallowed(board, i, cant_use);
-    for (char new = '1'; new <= '9'; new++) {
-        if (!cant_use[new - '0']) {
+    for (int new = 1; new <= 9; new++) {
+        if (!cant_use[new]) {
             board[i] = new;
             if (helper(board, i+1)) {
                 return true;
             }
-            board[i] = '0';
+            board[i] = 0;
         }
     }
     return false;
 }
 
+void parse_board(const char input[82], int output[81]) {
+    for (int i = 0; i < 81; i++) {
+        output[i] = input[i] - '0';
+    }
+}
+
+bool identical(const int board1[81], const int board2[81]) {
+    for (int i = 0; i < 81; i++) {
+        if (board1[i] != board2[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
 // Driver function.
-bool solve(char board[82]) { return helper(board, 0); }
+bool solve(int board[81]) { return helper(board, 0); }
 
 int main(int argc, [[maybe_unused]] char* argv[argc+1]) {
     const char* testcases[NUM_TESTCASES] = {
@@ -94,18 +114,24 @@ int main(int argc, [[maybe_unused]] char* argv[argc+1]) {
        "625178943948326157371945862257619384463587291189432576792863415516294738834751629",
     };
     for (int i = 0; i < NUM_TESTCASES; i++) {
-        char board[82];
-        strncpy(board, testcases[i], 82);
+        int board[81];
+        parse_board(testcases[i], board);
 
-        printf("Testcase %d:\n\nInput: %s\n", i+1, board);
-        printf("Expected: %s\n", expected[i]);
+        int expected_board[81];
+        parse_board(expected[i], expected_board);
+
+        printf("Testcase %d:\n\nInput: ", i+1);
+        print(board);
+        printf("\nExpected: %s\n", expected[i]);
         
         if (!solve(board)) {
-            printf("Actual: failure: could not solve\n");
-        } else if (strncmp(board, expected[i], 81) != 0) {
-            printf("Failure. Got: %s\n", board);
-        } else {
+            printf("Failure: could not solve\n");
+        } else if (identical(board, expected_board)) {
             printf("Success\n");
+        } else {
+            printf("Failure. Got: ");
+            print(board);
+            printf("\n");
         }
         printf("...\n");
     }
